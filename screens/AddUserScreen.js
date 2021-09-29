013b2600fb59f2618748c33196581bdf72207d80
HAFDIAHMED <ahafdi62@gmail.com>
2021-09-29 09:33:56 +0100

    add user screen

--- screens/AddUserScreen.js
+++ screens/AddUserScreen.js
@@ -4,92 +4,38 @@ import firebase  from "firebase/app";
 import "firebase/firestore";
 
 class AddUserScreen extends Component {
-  constructor() {
-    super();
-    this.dbRef = firebase.firestore().collection('users');
-    this.state = {
-      name: '',
-      email: '',
-      mobile: '',
-      isLoading: false
-    };
-  }
-
-  inputValueUpdate = (val, prop) => {
-    const state = this.state;
-    state[prop] = val;
-    this.setState(state);
-  }
-
-  storeUser() {
-    if(this.state.name === ''){
-     alert('Fill at least your name!')
-    } else {
-      this.setState({
-        isLoading: true,
-      });      
-      this.dbRef.add({
-        name: this.state.name,
-        email: this.state.email,
-        mobile: this.state.mobile,
-      }).then((res) => {
-        this.setState({
-          name: '',
-          email: '',
-          mobile: '',
-          isLoading: false,
-        });
-        this.props.navigation.navigate('UserScreen')
-      })
-      .catch((err) => {
-        console.error("Error found: ", err);
-        this.setState({
-          isLoading: false,
-        });
-      });
-    }
-  }
+  
 
   render() {
-    if(this.state.isLoading){
-      return(
-        <View style={styles.preloader}>
-          <ActivityIndicator size="large" color="#9E9E9E"/>
-        </View>
-      )
-    }
+   
     return (
       <ScrollView style={styles.container}>
-        <View style={styles.inputGroup}>
-          <TextInput
-              placeholder={'Name'}
-              value={this.state.name}
-              onChangeText={(val) => this.inputValueUpdate(val, 'name')}
-          />
-        </View>
-        <View style={styles.inputGroup}>
-          <TextInput
-              multiline={true}
-              numberOfLines={4}
-              placeholder={'Email'}
-              value={this.state.email}
-              onChangeText={(val) => this.inputValueUpdate(val, 'email')}
-          />
-        </View>
-        <View style={styles.inputGroup}>
-          <TextInput
-              placeholder={'Mobile'}
-              value={this.state.mobile}
-              onChangeText={(val) => this.inputValueUpdate(val, 'mobile')}
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            title='Add User'
-            onPress={() => this.storeUser()} 
-            color="#19AC52"
-          />
-        </View>
+       <View style={styles.inputgroup}>
+         {/*name */}
+         <TextInput 
+         multiline={true}
+          placeholder={'Name'}
+         />
+
+       </View>
+       <View style={styles.inputgroup}>
+         {/*email */}
+         <TextInput
+          placeholder={'Email'}
+         />
+       </View>
+       <View style={styles.inputgroup}>
+         <TextInput 
+         placeholder={"Mobile"}
+         />
+       </View>
+      
+       <View >
+         <Button
+          title="Add User"
+          color ='green'
+         />
+       </View>
       </ScrollView>
     );
   }
@@ -100,13 +46,11 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 35
   },
-  inputGroup: {
-    flex: 1,
-    padding: 0,
-    marginBottom: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
+   inputgroup:{
+     flex :1,
+     borderBottomWidth : 1,
+     borderBottomColor : '#cccccc'
+   },
   preloader: {
     left: 0,
     right: 0,
